7c752ae26b24dcb585fa7fbbdc55eaf4db5d6496
emmy27 <96811283+Emmy2721@users.noreply.github.com>
2024-04-14 01:44:41 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import http from 'node:http';
+
+
+http.createServer(req,res) {
+  console.log(req.url)
+  var path = req.url.toLowerCase();
+  switch(path) {
+    case '/':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('Home page');
+      break;
+    default:
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end('Not found');
+      break;
+    }
+}).listen(process.env.PORT || 3000);
